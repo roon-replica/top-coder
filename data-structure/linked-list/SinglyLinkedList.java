c8237148e97b194ecf6897b73be3bfb881a6a3d5
leemr <think-roon@naver.com>
2022-03-14 15:13:12 +0900

    [자료구조] singly linked list
    [자료구조] singly linked list
    
    [자료구조] singly linked list

---
+++ data-structure/linked-list/SinglyLinkedList.java
@@ -0,0 +1,96 @@
+import java.util.NoSuchElementException;
+
+public class SinglyLinkedList<T> {
+    int size;
+
+    Node<T> first, last;    // head, tail
+
+    public SinglyLinkedList() {
+    }
+
+//    public void addFirst(T content) {
+//        Node<T> newNode = new Node<>(content, first);
+//
+//        if (first == null) {
+//            first = newNode;
+//            last = newNode;
+//        } else {
+//            newNode.next = first;
+//            first = newNode;
+//        }
+//        size++;
+//    }
+
+    // REVIEW : 조금 다른 구현 논리 (라이브러리가 구현한 방식)
+    public void addFirst(T content) {
+        final Node<T> f = first;
+        final Node<T> newNode = new Node<>(content, f);
+        first = newNode;
+        newNode.next = f;
+
+        if(f == null) last = newNode;
+
+        size++;
+    }
+
+    //REVIEW : 많이 헷갈린다..
+    public void add(int idx, T content) {
+        if (size < idx) throw new IndexOutOfBoundsException();
+
+        Node<T> before = first;
+
+        for (int i = 0; i < idx - 1; i++) {
+            before = before.next;
+        }
+
+        if (idx == 0) {
+            Node<T> newNode = new Node(content, before);
+            first = newNode;
+        } else {
+            Node<T> newNode = new Node(content, before.next);
+            before.next = newNode;
+        }
+
+        size++;
+    }
+
+    public T removeFirst() {
+        final Node<T> f = first;
+        if (f == null) throw new NoSuchElementException();
+        first = f.next;
+        return f.content;
+    }
+
+    public T getFirst() {
+        if (first == null) throw new NoSuchElementException();
+        return first.content;
+    }
+
+    public T getLast() {
+        if (last == null) throw new NoSuchElementException();
+        return last.content;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private static class Node<T> {
+        T content;
+        Node<T> next;
+
+        Node(T content, Node<T> next) {
+            this.content = content;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+        for (Node<T> iterator = first; iterator != null; iterator = iterator.next) {
+            ret.append(iterator.content + " ");
+        }
+        return ret.toString();
+    }
+}
---
+++ data-structure/linked-list/SinglyLinkedListEntryPoint.java
@@ -0,0 +1,35 @@
+public class SinglyLinkedListEntryPoint {
+    public static void main(String[] args) {
+        // integer list
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        for (int i = 1; i <= 5; i++) {
+            list.addFirst(i);
+        }
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println(list.removeFirst());
+
+        System.out.println(list);
+        list.addFirst(1);
+        System.out.println(list);
+
+        // string list
+        SinglyLinkedList<String> list2 = new SinglyLinkedList<>();
+        for (char ch = 'a'; ch <= 'd'; ch++) {
+            list2.addFirst("" + ch);
+        }
+        System.out.println(list2);
+
+        // add(int idx, T content) 테스트
+        list2.add(0, "add-first");
+        list2.add(2, "add-2");
+        list2.add(list2.size(), "add-last");
+        list2.addFirst("add-first-again");
+        list2.add(list2.size(), "add-last-again");
+        System.out.println(list2);
+
+    }
+}
